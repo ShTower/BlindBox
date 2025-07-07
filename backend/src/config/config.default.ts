188da526316278a69919b30e73ca0a91d53e877b
ShTower <136039743+ShTower@users.noreply.github.com>
2025-07-07 20:33:15 +0800

    后端重做

--- backend/src/config/config.default.ts
+++
@@ -1,9 +0,0 @@
-import { MidwayConfig } from '@midwayjs/core';
-
-export default {
-  // use for cookie sign key, should change to your own and keep security
-  keys: '1751764620086_1502',
-  koa: {
-    port: 7001,
-  },
-} as MidwayConfig;
